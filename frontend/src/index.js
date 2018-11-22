543695c29cc5231a6c5fe555873d5e11b5732ae4
Zarkana <jsc940@gmail.com>
2018-11-22 14:20:38 -0800

    add react router

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import Main from "./Main";
 import './index.css';
+
+ReactDOM.render(
+    <Main/>, 
+    document.getElementById("root")
+  );
+
+//Tic tac toe
+
 
 function Square(props) {
     return (
@@ -67,7 +76,7 @@ class Game extends React.Component {
     jumpTo(step) {
         this.setState({
             stepNumber: step,
-            xIsNext: (step % 2) == 0,
+            xIsNext: (step % 2) === 0,
         });
     }
 
@@ -124,10 +133,10 @@ class Game extends React.Component {
 
 // ========================================
 
-ReactDOM.render(
-<Game />,
-document.getElementById('root')
-);
+// ReactDOM.render(
+// <Main />,
+// document.getElementById('root')
+// );
 
 function calculateWinner(squares) {
     const lines = [
