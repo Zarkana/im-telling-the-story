1af9a361fcba68b9d41a8de4e396dcfc1ec8db3b
Joseph <jsc940@gmail.com>
2019-01-05 16:42:06 -0800

    remove buttons... woops

--- frontend/src/Footer.js
+++ frontend/src/Footer.js
@@ -1,26 +1,15 @@
 import React, { Component } from "react";
  
+// constructor(props) {
+//   super 
+// }
+
 class Footer extends Component {
   render() {
     return (
       <footer>
           <div className="left shift-text-down">SIGN IN</div>
           <div className="center shift-text-down">
-            <button>
-              Jimmy
-            </button>
-            <button>
-              Rob
-            </button>
-            <button>
-              Sally
-            </button>
-            <button>
-              Martha
-            </button>
-            <button>
-              Logout
-            </button>
           </div>
           <div className="right shift-text-down">GITHUB</div>
       </footer>
